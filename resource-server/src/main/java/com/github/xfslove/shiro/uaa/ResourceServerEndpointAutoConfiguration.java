2be3543216236f4b02f26d4a55b9f51aa7d5d8d1
wongiven <xfslove@gmail.com>
2020-09-27 13:04:34 +0800

    release 1.0.6

--- resource-server/src/main/java/com/github/xfslove/shiro/uaa/ResourceServerEndpointAutoConfiguration.java
+++ resource-server/src/main/java/com/github/xfslove/shiro/uaa/ResourceServerEndpointAutoConfiguration.java
@@ -1,21 +1,19 @@
 package com.github.xfslove.shiro.uaa;
 
 import com.github.xfslove.shiro.uaa.endpoint.LogoutEndpoint;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 /**
  * Created by hanwen on 05/01/2018.
  */
+@EnableConfigurationProperties(ResourceServerProperties.class)
 @AutoConfigureAfter(ResourceServerShiroAutoConfiguration.class)
 public class ResourceServerEndpointAutoConfiguration {
 
-  @Autowired
-  private ResourceServerProperties resourceProperties;
-
   @Bean
-  public LogoutEndpoint logoutEndpoint() {
+  public LogoutEndpoint logoutEndpoint(ResourceServerProperties resourceProperties) {
     LogoutEndpoint logoutEndpoint = new LogoutEndpoint();
     logoutEndpoint.setServerScheme(resourceProperties.getServerScheme());
     logoutEndpoint.setServerHost(resourceProperties.getServerHost());
